b212facecd17abde5e6d868bd22085f4d809b380
Shuaib Ahmed <shuaib2001.sa@gmail.com>
2024-11-19 12:09:41 +0000

    writing tests

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,3 +1,17 @@
 import pytest
 
+@pytest.fixture(params=[i for i in range(2)])
+def data_set(request):
+    return request.param
+
+@pytest.fixture(params=[i for i in range(2)])
+def data_set2(request):
+    return request.param
+
 @pytest.fixture
+def order(data_set, data_set2):
+    return [data_set, data_set2]
+
+def test_data(order):
+    print(order)
+    pass
