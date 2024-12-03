8e00c4b5f4711354b03644250dc8f75b991bf0e3
shubini <shuaib2001.sa@gmail.com>
2024-12-03 21:41:30 +0000

    update

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -2,14 +2,17 @@ import pytest
 
 @pytest.fixture(params=[i for i in range(2)])
 def data_set(request):
+    print(request)
     return request.param
 
-@pytest.fixture(params=[i for i in range(2)])
+@pytest.fixture(params=[i for i in range(2, 4)])
 def data_set2(request):
+    print(request)
     return request.param
 
 @pytest.fixture
 def order(data_set, data_set2):
+    print(order)
     return [data_set, data_set2]
 
 def test_data(order):
