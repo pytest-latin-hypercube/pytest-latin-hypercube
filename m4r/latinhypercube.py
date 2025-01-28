daa8837ca9731ebbdcc75ac761ff264b6a99f1f8
shubini <shuaib2001.sa@gmail.com>
2025-01-28 18:28:05 +0000

    length test

--- m4r/latinhypercube.py
+++ m4r/latinhypercube.py
@@ -8,23 +8,23 @@ def LHparameterised():
 
 
 def LatinHypercube(args: dict, seed: int = 847, K: int = 1):
-    """Given a parameter space of N parameters each taking M values, return a Latin Hypercube sample."""
+    """Given a parameter space of M parameters each taking K values, return a Latin Hypercube sample."""
     np.random.seed = seed
     keys = []
     M = len(args)
-    N = len(list(args.values())[0])
-    samples = [[None for i in range(M)] for j in range(N)]
+    samples = [[None for _ in range(M)] for _ in range(N*K)]
 
-    for i, (key, value) in enumerate(args.items()):
-        if i == 0:
-            N = len(value)
-        keys.append(key)
-        if len(value) != N:
-            raise ValueError("All parameters' spaces are required to be the same length")
+    for k in range(K):
+        for i, (key, value) in enumerate(args.items()):
+            if i == 0:
+                N = len(value)
+            keys.append(key)
+            if len(value) != N:
+                raise ValueError("All parameters' spaces are required to be the same length")
 
-        shuffled_intervals = np.random.permutation(value)
-        for j, val in enumerate(shuffled_intervals):
-            samples[j][i] = val
+            shuffled_intervals = np.random.permutation(value)
+            for j, val in enumerate(shuffled_intervals):
+                samples[j][k*N + i] = val
 
     return keys, samples
 
@@ -40,21 +40,22 @@ def LatinHyperRectangle(args: dict, seed: int = 847, K: int = 1):
         lengths.append(len(value))
     
     N = max(lengths)
-    samples = [[None for i in range(M)] for j in range(N)]
+    samples = [[None for _ in range(M)] for _ in range(N)]
 
-    for i, (key, value) in enumerate(args.items()):
-        keys.append(key)
-        shuffled_intervals = []
-        if len(value) < N:
-            curr_length = len(value)
-            while curr_length < N:
-                shuffled_intervals.extend(np.random.permutation(value))
-            shuffled_intervals = shuffled_intervals[:N]    
-        else:
-            shuffled_intervals = np.random.permutation(value)
+    for k in range(K):
+        for i, (key, value) in enumerate(args.items()):
+            keys.append(key)
+            shuffled_intervals = []
+            if len(value) < N:
+                curr_length = len(value)
+                while curr_length < N:
+                    shuffled_intervals.extend(np.random.permutation(value))
+                    curr_length = len(shuffled_intervals)
+                shuffled_intervals = shuffled_intervals[:N]    
+            else:
+                shuffled_intervals = np.random.permutation(value)
 
-        
-        for j, val in enumerate(shuffled_intervals):
-            samples[j][i] = val
+            for j, val in enumerate(shuffled_intervals):
+                samples[j][k*N + i] = val
 
     return keys, samples
