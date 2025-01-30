56c926852ed8b37cfbf7328f0bc6f51a0ed5b7d1
shubini <shuaib2001.sa@gmail.com>
2025-01-30 15:18:43 +0000

    fix parameter names

--- m4r/latinhypercube.py
+++ m4r/latinhypercube.py
@@ -3,59 +3,59 @@ import numpy as np
 
 def LHparameterised(args: dict, seed: int = 847):
     def wrapper(test_func):
-        argnames, argvalues = LatinHypercube(args, seed=seed)
+        argnames, argvalues = latin_hyperrectangle(args, seed=seed)
         return pytest.mark.parametrize(argnames, argvalues)(test_func)
     return wrapper
 
 
-def LatinHypercube(args: dict, seed: int = 847, K: int = 1):
-    """Given a parameter space of M parameters each taking K values, return a Latin Hypercube sample."""
-    
-    keys = []
-    M = len(args)
-    np.random.seed(seed)
+def latin_hypercube(args: dict, seed: int = 847, number_of_iterations: int = 1):
+    """Given a parameter space of number_of_arguments parameters each taking N values, return a Latin Hypercube sample."""
 
-    for k in range(K):
+    keys = []
+    number_of_arguments = len(args)
+    np.random.seed(seed) # 
+
+    for k in range(number_of_iterations):
         for i, (key, value) in enumerate(args.items()):
             if i == 0:
-                N = len(value)
-                samples = [[None for _ in range(M)] for _ in range(N*K)]
+                longest_parameter_space = len(value)
+                samples = [[None for _ in range(number_of_arguments)] for _ in range(longest_parameter_space*number_of_iterations)]
             keys.append(key)
-            if len(value) != N:
+            if len(value) != longest_parameter_space:
                 raise ValueError("All parameters' spaces are required to be the same length")
             shuffled_intervals = np.random.permutation(value)
             for j, val in enumerate(shuffled_intervals):
-                samples[j][k*N + i] = val
+                samples[k*longest_parameter_space + i][j]= val
 
     return keys, samples
 
 
-def LatinHyperRectangle(args: dict, seed: int = 847, K: int = 1):
-    """Given a parameter space of N parameters each taking M values, return a Latin Hypercube sample."""
+def latin_hyperrectangle(args: dict, seed: int = 847, number_of_iterations: int = 1):
+    """Given a parameter space of longest_parameter_space parameters each taking number_of_arguments values, return a Latin Hypercube sample."""
     keys = []
-    M = len(args)
+    number_of_arguments = len(args) # give variables better names
     lengths = []
-    np.random.seed(seed)
+    np.random.seed(seed) # change this to something non-global
     for i, (key, value) in enumerate(args.items()):
         keys.append(key)
         lengths.append(len(value))
 
-    N = max(lengths)
-    samples = [[None for _ in range(M)] for _ in range(N)]
+    longest_parameter_space = max(lengths)
+    samples = [[None for _ in range(number_of_arguments)] for _ in range(number_of_iterations*longest_parameter_space)]
 
-    for k in range(K):
+    for k in range(number_of_iterations): #use zip and make this make more sense
         for i, (key, value) in enumerate(args.items()):
-            shuffled_intervals = []
-            if len(value) < N:
+            if len(value) < longest_parameter_space:
+                shuffled_intervals = []
                 curr_length = len(value)
-                while curr_length < N:
+                while curr_length < longest_parameter_space:
                     shuffled_intervals.extend(np.random.permutation(value))
                     curr_length = len(shuffled_intervals)
-                shuffled_intervals = shuffled_intervals[:N]    
+                shuffled_intervals = shuffled_intervals[:longest_parameter_space]
             else:
                 shuffled_intervals = np.random.permutation(value)
 
             for j, val in enumerate(shuffled_intervals):
-                samples[j][k*N + i] = val
+                samples[k*longest_parameter_space + i][j] = val
 
     return keys, samples
