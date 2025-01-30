08b9ef7b2cc1d3db3f8905f7c6adb7b3cb8ee714
shubini <shuaib2001.sa@gmail.com>
2025-01-30 11:35:04 +0000

    Much easier (and better) solution to seeding

--- m4r/latinhypercube.py
+++ m4r/latinhypercube.py
@@ -13,7 +13,7 @@ def LatinHypercube(args: dict, seed: int = 847, K: int = 1):
     
     keys = []
     M = len(args)
-    
+    np.random.seed(seed)
 
     for k in range(K):
         for i, (key, value) in enumerate(args.items()):
@@ -23,23 +23,23 @@ def LatinHypercube(args: dict, seed: int = 847, K: int = 1):
             keys.append(key)
             if len(value) != N:
                 raise ValueError("All parameters' spaces are required to be the same length")
-            np.random.seed(seed*(k+1))
             shuffled_intervals = np.random.permutation(value)
             for j, val in enumerate(shuffled_intervals):
                 samples[j][k*N + i] = val
 
     return keys, samples
 
+
 def LatinHyperRectangle(args: dict, seed: int = 847, K: int = 1):
     """Given a parameter space of N parameters each taking M values, return a Latin Hypercube sample."""
     keys = []
     M = len(args)
     lengths = []
-
+    np.random.seed(seed)
     for i, (key, value) in enumerate(args.items()):
         keys.append(key)
         lengths.append(len(value))
-    
+
     N = max(lengths)
     samples = [[None for _ in range(M)] for _ in range(N)]
 
@@ -48,15 +48,11 @@ def LatinHyperRectangle(args: dict, seed: int = 847, K: int = 1):
             shuffled_intervals = []
             if len(value) < N:
                 curr_length = len(value)
-                count = 0 # to avoid repeated permutations
                 while curr_length < N:
-                    count += 1
-                    np.random.seed(seed*count*(k+1))
                     shuffled_intervals.extend(np.random.permutation(value))
                     curr_length = len(shuffled_intervals)
                 shuffled_intervals = shuffled_intervals[:N]    
             else:
-                np.random.seed(seed*(k+1))
                 shuffled_intervals = np.random.permutation(value)
 
             for j, val in enumerate(shuffled_intervals):
