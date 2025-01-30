006cbff108d50cb06bf9fda5577b4b50e439d526
shubini <shuaib2001.sa@gmail.com>
2025-01-30 16:26:09 +0000

    improving hyperrectangle with zip and writing number of iteration tests

--- m4r/latinhypercube.py
+++ m4r/latinhypercube.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def LHparameterised(args: dict, seed: int = 847):
     def wrapper(test_func):
-        argnames, argvalues = latin_hyperrectangle(args, seed=seed)
+        argnames, argvalues = latin_hypercube(args, seed=seed)
         return pytest.mark.parametrize(argnames, argvalues)(test_func)
     return wrapper
 
@@ -12,50 +12,43 @@ def latin_hypercube(args: dict, seed: int = 847, number_of_iterations: int = 1):
     """Given a parameter space of number_of_arguments parameters each taking N values, return a Latin Hypercube sample."""
 
     keys = []
-    number_of_arguments = len(args)
-    np.random.seed(seed) # 
+    np.random.seed(seed)
 
-    for k in range(number_of_iterations):
-        for i, (key, value) in enumerate(args.items()):
-            if i == 0:
-                longest_parameter_space = len(value)
-                samples = [[None for _ in range(number_of_arguments)] for _ in range(longest_parameter_space*number_of_iterations)]
-            keys.append(key)
-            if len(value) != longest_parameter_space:
-                raise ValueError("All parameters' spaces are required to be the same length")
-            shuffled_intervals = np.random.permutation(value)
-            for j, val in enumerate(shuffled_intervals):
-                samples[k*longest_parameter_space + i][j]= val
+    for i, (key, value) in enumerate(args.items()):
+        if i == 0:
+            longest_parameter_space = len(value)
+            perms = []
+        keys.append(key)
+        if len(value) != longest_parameter_space:
+            raise ValueError("All parameters' spaces are required to be the same length")
+        perm = np.random.permutation(value*number_of_iterations)
+        perms.append(perm)
 
-    return keys, samples
+    return keys, list(zip(*perms))
 
 
 def latin_hyperrectangle(args: dict, seed: int = 847, number_of_iterations: int = 1):
     """Given a parameter space of longest_parameter_space parameters each taking number_of_arguments values, return a Latin Hypercube sample."""
     keys = []
-    number_of_arguments = len(args) # give variables better names
-    lengths = []
+    longest_parameter_space = 0
     np.random.seed(seed) # change this to something non-global
-    for i, (key, value) in enumerate(args.items()):
+    for key, value in args.items():
         keys.append(key)
-        lengths.append(len(value))
+        if len(value) > longest_parameter_space:
+            longest_parameter_space = len(value)
 
-    longest_parameter_space = max(lengths)
-    samples = [[None for _ in range(number_of_arguments)] for _ in range(number_of_iterations*longest_parameter_space)]
+    perms = []
 
-    for k in range(number_of_iterations): #use zip and make this make more sense
-        for i, (key, value) in enumerate(args.items()):
-            if len(value) < longest_parameter_space:
-                shuffled_intervals = []
-                curr_length = len(value)
-                while curr_length < longest_parameter_space:
-                    shuffled_intervals.extend(np.random.permutation(value))
-                    curr_length = len(shuffled_intervals)
-                shuffled_intervals = shuffled_intervals[:longest_parameter_space]
-            else:
-                shuffled_intervals = np.random.permutation(value)
+    for key, value in args.items():
+        if len(value) < longest_parameter_space:
+            perm = []
+            curr_length = len(value)
+            while curr_length < longest_parameter_space*number_of_iterations:
+                perm.extend(np.random.permutation(value))
+                curr_length = len(perm)
+            perm = perm[:longest_parameter_space*number_of_iterations]
+        else:
+            perm = np.random.permutation(value*number_of_iterations)
+        perms.append(perm)
 
-            for j, val in enumerate(shuffled_intervals):
-                samples[k*longest_parameter_space + i][j] = val
-
-    return keys, samples
+    return keys, list(zip(*perms))
--- tests/test_latinhypercube.py
+++ tests/test_latinhypercube.py
@@ -2,11 +2,12 @@ import pytest
 import m4r.latinhypercube
 import inspect
 
+
 @pytest.mark.parametrize("x", [{"x": [0, 1, 2]}, {"x": [0]}, {"x": [0, 1, 2], "y": [0, 1, 2]}, {"x": []}, {"x": [0, 1, 2, 4], 2: [0, 1, 2, 3], "z": [0, 2, 5, 1]}])
 def test_latinhypercube_length(x):
     M = len(x)
     N = len(next(iter(x.values()), []))
-    keys, values = m4r.latinhypercube.LatinHypercube(x)
+    keys, values = m4r.latinhypercube.latin_hypercube(x)
     assert len(keys) == M
     assert len(values) == N
     if len(values) > 0:
@@ -16,15 +17,26 @@ def test_latinhypercube_length(x):
 @pytest.mark.parametrize("seed", [847, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 def test_latinhypercube_seed(seed):
     x = {"x": [0, 1, 2], "y": [0, 1, 2], "z": [0, 1, 2]}
-    keys, values = m4r.latinhypercube.LatinHypercube(x, seed=seed)
-    keys2, values2 = m4r.latinhypercube.LatinHypercube(x, seed=seed)
+    keys, values = m4r.latinhypercube.latin_hypercube(x, seed=seed)
+    keys2, values2 = m4r.latinhypercube.latin_hypercube(x, seed=seed)
     assert keys == keys2
     assert values == values2
 
 
+@pytest.mark.parametrize("k", [1, 2, 3, 4, 5, 6, 7, 8, 9])
+def test_latinhypercube_number_of_iterations(k):
+    x = {"x": [0, 1, 2], "y": [0, 1, 2], "z": [0, 1, 2]}
+    M = len(x)
+    N = len(next(iter(x.values()), []))
+    keys, values = m4r.latinhypercube.latin_hypercube(x, number_of_iterations=k)
+    assert len(keys) == M
+    assert len(values) == N*k
+    if len(values) > 0:
+        assert len(values[0]) == M
+
 @pytest.mark.parametrize("x", [{"x": [0, 1, 2]}, {"x": [0, 1, 2], "y": [0, 1, 2]}, {"x": []}, {"x": [0, 1, 2, 4], 2: [0, 1, 2, 3], "z": [0, 2, 5, 1]}])
 def test_latinhypercube_latinproperty(x):
-    argnames, argvalues = m4r.latinhypercube.LatinHypercube(x)
+    argnames, argvalues = m4r.latinhypercube.latin_hypercube(x)
     for i, arg in enumerate(argnames):
         values = [val for val in x[arg]]
         values2 = [argvalues[j][i] for j in range(len(argvalues))]
@@ -41,10 +53,10 @@ def test_latinhypercube_latinproperty(x):
 def test_latinhyperrectangle_lengths(x):
     M = len(x)
     N = len(next(iter(x.values()), []))
-    for key, value in x.items():
+    for value in x.values():
         if len(value) > N:
             N = len(value)
-    keys, values = m4r.latinhypercube.LatinHyperRectangle(x)
+    keys, values = m4r.latinhypercube.latin_hyperrectangle(x)
     assert len(keys) == M
     assert len(values) == N
     if len(values) > 0:
@@ -54,17 +66,31 @@ def test_latinhyperrectangle_lengths(x):
 @pytest.mark.parametrize("seed", [847 + i for i in range(10)])
 def test_latinhyperrectangle_seed(seed):
     x = {"x": [0, 1], "y": [0, 1, 2], "z": [0, 1, 2]}
-    keys, values = m4r.latinhypercube.LatinHyperRectangle(x, seed=seed)
-    keys2, values2 = m4r.latinhypercube.LatinHyperRectangle(x, seed=seed)
+    keys, values = m4r.latinhypercube.latin_hyperrectangle(x, seed=seed)
+    keys2, values2 = m4r.latinhypercube.latin_hyperrectangle(x, seed=seed)
     assert keys == keys2
     assert values == values2
 
+
+@pytest.mark.parametrize("k", [1, 2, 3, 4, 5, 6, 7, 8, 9])
+def test_latinhyperrectangle_number_of_iterations(k):
+    x = {"x": [0, 1, 2], "y": [0, 1, 2], "z": [0, 1, 2]}
+    M = len(x)
+    N = len(next(iter(x.values()), []))
+    for value in x.values():
+        if len(value) > N:
+            N = len(value)
+    keys, values = m4r.latinhypercube.latin_hyperrectangle(x, number_of_iterations=k)
+    assert len(keys) == M
+    assert len(values) == N*k
+    if len(values) > 0:
+        assert len(values[0]) == M
 
 
 @pytest.mark.parametrize(["func", "args"], [[lambda x, y : x + y, [[1, 2, 3], [4, 5, 6]]]])
 def test_output_is_valid_input(func, args):
     args_dict = inspect.getcallargs(func, *args)
-    argnames, argvalues = m4r.latinhypercube.LatinHypercube(args_dict)
+    argnames, argvalues = m4r.latinhypercube.latin_hypercube(args_dict)
     try:
         pytest.mark.parametrize(argnames, argvalues)(func)
         assert True
